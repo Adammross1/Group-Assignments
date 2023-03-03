32491c7df9575936fc443a8a836d2f9b1a5883c8
Adammross1 <adamross202@gmail.com>
2023-03-03 10:33:04 -0700

    added some spacing

--- hamburger.py
+++ hamburger.py
@@ -1,8 +1,10 @@
 # Authors: Isaiah Strong, Adam Ross, Brandon Cragun, Caden Plewe, Gibson Bailey
 # Description: This program will track how many hamburgers each driver (customer) delivers
+
 #this will import random number range
 import random
 from queue import Queue
+
 #this class will collect orders
 class Order:
     #this constructor creates an instance variable for burger_count
@@ -12,6 +14,7 @@ class Order:
     #this method will assign a random value to the number of burgers
     def randomBurgers(self):
         return random.randrange(1,20)
+    
 #this class will create a list of customer names
 class Person(Order):
     def __init__(self, burger_count,customer_name):
@@ -22,18 +25,22 @@ class Person(Order):
         asCustomers = ["Jefe", "El Guapo", "Lucky Day", "Ned Nederlander", "Dusty Bottoms",
                        "Harry Flugleman", "Carmen", "Invisible Swordsman", "Singing Bush"]
         return random.choice(asCustomers)
+    
 # this class will inherit from the Person class
 class Customer(Person):
     def __init__(self, customer_name,burger_count, order):
         super().__init__(customer_name,burger_count)
         self.order = order
         order = burger_count
+
 # Initializing a queue able to handle 100 drivers
 q = Queue(maxsize = 100)
-# creates a dictionary to store customer name and amt of burgers ordered
+
+# creates a dictionary to store customer name and amount of burgers ordered
 customerDict = {
   Person.customer_name:Order.burger_count
 }
+
 #this variable will pass values into the Order class
 order1 = Order(0)
 print(Order.randomBurgers(order1))
